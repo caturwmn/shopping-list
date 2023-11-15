4ba15cb6273deae0bc05c00fb7f05f4be45431bb
caturwmn <caturwira41@gmail.com>
2023-11-15 13:28:04 +0700

    Mencoba ulang deploy

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -35,6 +35,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+# CORS SETTINGS
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
+
 
 # Application definition
 
@@ -46,6 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -56,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'shopping_list.urls'
